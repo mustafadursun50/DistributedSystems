92c08f7a5079f0562113ef5ef0a12e2893c24431
Arnaud <arnaudzobel1@yahoo.fr>
2020-06-27 16:11:19 +0200

    implement client reservation

--- src/main/java/de/hhz/distributed/system/handlers/ClientMessageHandler.java
+++ src/main/java/de/hhz/distributed/system/handlers/ClientMessageHandler.java
@@ -2,6 +2,8 @@ package de.hhz.distributed.system.handlers;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,7 +22,7 @@ public class ClientMessageHandler implements Runnable {
 	private Server server;
 	Timer mProductTimer;
 	private TimerTask timerTask;
-	
+
 	public ClientMessageHandler(String input, Socket socket, Server server) {
 		this.inputMsg = input;
 		this.sender = new Sender();
@@ -30,11 +32,11 @@ public class ClientMessageHandler implements Runnable {
 	}
 
 	private void lockProductTimer() {
-		if(mProductTimer != null) {
+		if (mProductTimer != null) {
 			mProductTimer.cancel();
 			System.out.println("ProductTimer canceld");
 		}
-		
+
 		timerTask = new TimerTask() {
 
 			@Override
@@ -42,7 +44,7 @@ public class ClientMessageHandler implements Runnable {
 				String msgToSend = ProductDb.getCurrentData();
 				sender.sendMultiCastMessage(msgToSend, Constants.CLIENT_MULTICAST_ADDRESS,
 						Constants.CLIENT_MULTICAST_PORT);
-				System.out.println("Timer fertig: "+ msgToSend);
+				System.out.println("Timer fertig: " + msgToSend);
 				mProductTimer.cancel();
 			}
 		};
@@ -50,9 +52,8 @@ public class ClientMessageHandler implements Runnable {
 		mProductTimer.schedule(timerTask, 10000);
 		System.out.println("mProductTimer: " + mProductTimer);
 
-
 	}
-	
+
 	public void run() {
 		try {
 			if (inputMsg.startsWith(Constants.PACKAGE_LOSS)) {
@@ -63,37 +64,77 @@ public class ClientMessageHandler implements Runnable {
 					System.out.println("ERROR: askedMessage not successfully sent");
 				}
 
-			} 
-			else if (inputMsg.startsWith("reserve")) {
+			} else if (inputMsg.startsWith("reserve")) {
 				// Multicast an Gruppe mit ( "bananaLock" )
 				System.out.println("IN RESERVE");
-				this.sender.sendMultiCastMessage(this.server.getPort()+",lockBanane", Constants.CLIENT_MULTICAST_ADDRESS,
-						Constants.CLIENT_MULTICAST_PORT);
+				this.sender.sendMultiCastMessage(this.server.getPort() + ",lockBanane",
+						Constants.CLIENT_MULTICAST_ADDRESS, Constants.CLIENT_MULTICAST_PORT);
 				lockProductTimer();
 				System.out.println("mProductTimerAfter: " + mProductTimer);
 
-				// auf TCP Message antworten mit     reservieren erfolgreich 
-				sender.sendTCPMessage(Constants.RESERVESUCCES, this.socket);
-				
-				
+				String[] order = inputMsg.split(",");
+				int bananaReq = Integer.parseInt(order[1]);
+				int milkReq = Integer.parseInt(order[2]);
+				int tomateReq = Integer.parseInt(order[3]);
+
+				String products = Files.readAllLines(Paths.get(Constants.PRODUCT_DB_NAME)).get(0);
+				String[] splitedDb = products.split(",");
+				int bananaDb = Integer.parseInt(splitedDb[0]);
+				int milkDb = Integer.parseInt(splitedDb[1]);
+				int tomatoDb = Integer.parseInt(splitedDb[2]);
+
+				if (bananaReq > 0) {
+					// Reserver banana
+					if (bananaDb >= bananaReq) {
+						if (bananaReq + 3 <= bananaDb) {
+							sender.sendTCPMessage("banana,reservation,OK,tomato," + 3, this.socket);
+						} else {
+							sender.sendTCPMessage("banana,reservation, OK", this.socket);
+						}
+					} else {
+						sender.sendTCPMessage("banana,NOK," + bananaDb, this.socket);
+					}
+
+				} else if (tomateReq > 0) {
+					// Reserver tomato
+					if (tomateReq >= tomatoDb) {
+						if (tomateReq + 3 <= tomatoDb) {
+							sender.sendTCPMessage("tomato,reservation,OK,banana," + 3, this.socket);
+						} else {
+							sender.sendTCPMessage("tomato,reservation, OK", this.socket);
+						}
+					} else {
+						sender.sendTCPMessage("tomato,reservation,NOK," + bananaDb, this.socket);
+					}
+				} else if (milkReq > 0) {
+					// Reserve milk
+					if (milkReq >= milkDb) {
+						if (milkReq + 2 <= milkDb) {
+							sender.sendTCPMessage("milk,reservation,OK,tomato," + 2, this.socket);
+						} else {
+							sender.sendTCPMessage("milk,reservation, OK", this.socket);
+						}
+					} else {
+						sender.sendTCPMessage("milk,reservation,NOK," + milkDb, this.socket);
+					}
+				}
 			}
 
-			
 			else if (inputMsg.startsWith("requestOrder")) {
 				if (ProductDb.updateProductDb(this.inputMsg)) {
 					sender.sendTCPMessage("responseOrder,OK", this.socket);
 					Thread.sleep(100);
-					
-					System.out.println("input: "+ this.inputMsg + " lock " + mProductTimer);
 
-					if(this.inputMsg.equals("requestOrder,1,0,0") && (mProductTimer != null)) {
+					System.out.println("input: " + this.inputMsg + " lock " + mProductTimer);
+
+					if (this.inputMsg.equals("requestOrder,1,0,0") && (mProductTimer != null)) {
 						mProductTimer.cancel();
 						System.out.println("Product timer canceled");
 						sender.sendTCPMessage(Constants.DISCOUNT, this.socket);
 						return;
 					}
-					
-					//String msgToSend = FifoDeliver.assigneSequenceId(ProductDb.getCurrentData());
+
+					// String msgToSend = FifoDeliver.assigneSequenceId(ProductDb.getCurrentData());
 					String msgToSend = ProductDb.getCurrentData();
 					this.sender.sendMultiCastMessage(msgToSend, Constants.CLIENT_MULTICAST_ADDRESS,
 							Constants.CLIENT_MULTICAST_PORT);
@@ -103,8 +144,7 @@ public class ClientMessageHandler implements Runnable {
 					sb.append(",");
 					sb.append(ProductDb.getCurrentData());
 					this.updateReplicats(sb.toString());
-				} 
-				else {
+				} else {
 					sender.sendTCPMessage("responseOrder,NOK", this.socket);
 
 				}
